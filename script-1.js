6700ae428ded4281b66bf3a351954d536c896ac8
Ben <kyutheoriginal@gmail.com>
2022-04-29 16:27:39 +0200

    deleteBooks working

--- script-1.js
+++ script-1.js
@@ -1,12 +1,12 @@
 var myBooks = [
   {
-    id: 1,
+    id: 0,
     title: "Harry Potter and the Deathly Hallows",
     author: "J. K. Rowling",
     cover: "http://covers.openlibrary.org/b/isbn/3551354073-M.jpg" 
   },
   {
-    id: 2,
+    id: 1,
     title: "Food and feeding",
     author: "Sir Henry Thompson",
     cover: "http://covers.openlibrary.org/b/isbn/0543994767-M.jpg" 
@@ -16,33 +16,53 @@ var myBooks = [
 
 function displayBooksFromMyBooksArray() { 
   for(var i = 0; i < myBooks.length; i++) {
-
     var titel = document.createElement('li');    
     titel.style.listStyle = 'none';    
     titel.innerText = myBooks[i].title;
+    titel.classList = 'title';
     document.body.appendChild(titel);
 
     var img = document.createElement('img');
     img.src = myBooks[i].cover;
-    document.body.appendChild(img);
-    
+    img.classList = 'image'
+    document.body.appendChild(img);    
 
     var auth = document.createElement('li');
     auth.style.listStyle = 'none';
     auth.innerText = "by " + myBooks[i].author;     
+    auth.classList = 'auth';
     document.body.appendChild(auth); 
 
     var delButton = document.createElement('button');
     delButton.innerText = 'delete';
     delButton.style.display = 'block';
     delButton.style.marginBottom = '25px'
-    delButton.classList = 'delete';
+    delButton.classList = 'delete';    
     document.body.appendChild(delButton);
   }
 }
 displayBooksFromMyBooksArray()
 
 
+function deleteBooksFromMyBooksArray() {
+  let delButton = document.querySelectorAll(".delete");
+  delButton.forEach(function(books, index) {
+    let img = document.getElementsByClassName ('image')[index];
+    let auth = document.querySelectorAll('.auth')[index];
+    let delButtonT = document.querySelectorAll(".delete")[index];
+    let title = document.querySelectorAll(".title")[index];
+
+    books.onclick = function() {
+      myBooks.splice(index, 1);
+      console.log(myBooks);      
+      img.remove();
+      auth.remove();
+      delButtonT.remove();
+      title.remove();
+    }
+  }) 
+}
+deleteBooksFromMyBooksArray()
 
 
 
@@ -65,100 +85,6 @@ function addBooksToMyBooksArray() {
     }})}
 
 
-
-/* 
- 
-    var img = document.createElement('img'); 
-    img.src = document.querySelectorAll("img").src;   
-    myBooks.push(img.src);
-    displayBooksFromMyBooksArray()
-    console.log(myBooks);  
-
-    var auth = document.createElement('li');
-    auth.style.listStyle = 'none';
-    auth.innerText = document.querySelectorAll("label")[ind+1].innerText;      
-    document.body.appendChild(auth);
-
-    var page = document.createElement('input');
-    page.type = 'text';
-    document.body.appendChild(page);
-
-    var delButton = document.createElement('button');
-    delButton.innerText = 'delete';
-    delButton.style.display = 'block';
-    delButton.style.marginBottom = '25px'
-    delButton.classList = 'delete';
-    document.body.appendChild(delButton)
-  } */
-
-
-
-
-
-
-/* function addBooksToMyBooksArray() {
-  let cover = document.querySelectorAll("button.addToMyReadingList");
-  cover.forEach(function(cv, ind) {
-    cv.onclick = function saveCover() {      
-      var img = document.createElement('img'); 
-      img.src = document.querySelectorAll("img")[ind].src;   
-      myBooks.push(img.src);
-      console.log(myBooks);      
-
-      var auth = document.createElement('li');
-      auth.style.listStyle = 'none';
-      auth.innerText = document.querySelectorAll("label")[ind+1].innerText;      
-      document.body.appendChild(auth);
-
-      var page = document.createElement('input');
-      page.type = 'text';
-      document.body.appendChild(page);
-      
-      var delButton = document.createElement('button');
-      delButton.innerText = 'delete';
-      delButton.style.display = 'block';
-      delButton.style.marginBottom = '25px'
-      delButton.classList = 'delete';
-      document.body.appendChild(delButton)
-    }
-  })
-} */
-
-/* function displayBooksFromMyBooksArray() { 
-  var img = document.createElement('img'); 
-  img.src = document.querySelectorAll("img")[ind].src;   
-  document.body.appendChild(img);      
-
-  var auth = document.createElement('li');
-  auth.style.listStyle = 'none';
-  auth.innerText = document.querySelectorAll("label")[ind+1].innerText;      
-  document.body.appendChild(auth);
-
-  var page = document.createElement('input');
-  page.type = 'text';
-  document.body.appendChild(page);
-  
-  var delButton = document.createElement('button');
-  delButton.innerText = 'delete';
-  delButton.style.display = 'block';
-  delButton.style.marginBottom = '25px'
-  delButton.classList = 'delete';
-  document.body.appendChild(delButton);
-
-  
-  //add from array
-  let cover = document.querySelectorAll("button.addToMyReadingList");
-  cover.forEach(function(cv, ind) {
-    cv.onclick = function saveCover() {
-      //add from (last in) array
-    }
-  })
-} */
-
-
-
-
-//just a SEARCH DISPLAY
 function queryOpenLibrary() {
   document.getElementById('output').innerHTML="";
   fetch("http://openlibrary.org/search.json?q="+document.getElementById("input").value)
@@ -171,26 +97,15 @@ function queryOpenLibrary() {
           +"<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-M.jpg'><br>"
           +"<button class='addToMyReadingList'>add to my reading list</button>"
       }
-  addBooksToMyBooksArray()
+    addBooksToMyBooksArray()
+    deleteBooksFromMyBooksArray()
   })
 }
 
 
-
-
-
-function deleteElement() {
-  // target all individual del button using their class 'delete'
-  // add function, that targets the content above it
-  // idea: first, write a function, that saves content to an array. That way, you only have to target them within the array
-}
-
-// structure
-// ONE function that adds books, and only books from array get added
 
 // tasks:
 // 1. add disabled to buttons
 // 2. prevent code from adding book thats already on the list
 // 3. save data in array
 // 4. reccomendation (add favorite books)
-
